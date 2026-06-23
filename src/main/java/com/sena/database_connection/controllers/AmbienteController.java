f879346f4f45aa46f83a40d479758cea53209cac
alejositoau <dmpatinoa2@gmail.com>
2026-06-23 13:31:01 -0500

    Verificaciones

--- src/main/java/com/sena/database_connection/controllers/AmbienteController.java
+++ src/main/java/com/sena/database_connection/controllers/AmbienteController.java
@@ -1,17 +1,25 @@
 package com.sena.database_connection.controllers;
 
-import com.sena.database_connection.model.Ambiente;
-import com.sena.database_connection.service.AmbienteService;
-import com.sena.database_connection.service.ReservaService;
-import jakarta.validation.Valid;
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
-import java.util.List;
+import com.sena.database_connection.model.Ambiente;
+import com.sena.database_connection.service.AmbienteService;
+import com.sena.database_connection.service.ReservaService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/ambientes")
